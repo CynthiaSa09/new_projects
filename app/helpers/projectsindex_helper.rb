353820449f2529be98fce9c021e3a9719b5c6fad
Cynthia <cinthya3259@gmail.com>
2021-03-04 16:52:55 -0600

    Finish

---
+++ app/helpers/projectsindex_helper.rb
@@ -0,0 +1,2 @@
+module ProjectsindexHelper
+end
---
+++ test/controllers/projectsindex_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProjectsindexControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
